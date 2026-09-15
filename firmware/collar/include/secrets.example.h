d8b7ad15344038e96d8d531912bd9733fe27e4f2
nanato0818 <pxiaxiang@gmail.com>
2026-09-15 18:17:37 +0900

    Add Wi-Fi configuration template

---
+++ firmware/collar/include/secrets.example.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// このファイルを secrets.h として複製し、実際の接続情報を入力してください。
+// secrets.h はGit管理されません。
+constexpr char WIFI_SSID[] = "YOUR_WIFI_SSID";
+constexpr char WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
